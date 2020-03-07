cf6c7deff5cf2fb61859b46b401b0b89ac159ecc
kvlahov <vlahov.kresimir@gmail.com>
2020-03-07 20:33:06 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# OOPNET_projekt
---
+++ Utilities/ApiHelper.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public class ApiHelper
+    {
+        public string Url { get; set; }
+
+        public ApiHelper(string url)
+        {
+            Url = url;
+        }
+
+        private async Task<string> GetAsJsonAsync(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(url);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsStringAsync();
+
+                }
+                return null;
+            }
+        }
+
+        public async Task<List<T>> GetDataList<T>(string url = null)
+        {
+            url = url ?? Url;
+
+            var data = await GetAsJsonAsync(url);
+            return JsonConvert.DeserializeObject<List<T>>(data);
+        }
+    }
+}
