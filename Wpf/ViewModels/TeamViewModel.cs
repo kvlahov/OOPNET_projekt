58ea4d58b2c079f96d47a348c4d4692a36aac901
Kresimir Vlahov <kresimir.vlahov@ericsson.com>
2020-04-18 19:31:00 +0200

    Play with wpf

---
+++ Wpf/ViewModels/TeamViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf.ViewModels
+{
+    class TeamViewModel
+    {
+        public string Name { get; set; }
+        public string FifaCode { get; set; }
+        public int MatchesPlayed { get; set; }
+        public int MatchesWon { get; set; }
+        public int MatchesLost { get; set; }
+        public int MatchesDraw { get; set; }
+        public int GoalsScored { get; set; }
+        public int GoalsTaken { get; set; }
+        public int GoalsDifference { get; set; }
+
+    }
+}
