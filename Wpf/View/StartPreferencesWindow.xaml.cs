407309e07e7e3a09956c2a43fe5bb02b24b49888
Kresimir Vlahov <kresimir.vlahov@ericsson.com>
2020-04-18 12:09:16 +0200

    Add wpf project

---
+++ Wpf/View/StartPreferencesWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Wpf.View
+{
+    /// <summary>
+    /// Interaction logic for StartPreferencesWindow.xaml
+    /// </summary>
+    public partial class StartPreferencesWindow : Window
+    {
+        public StartPreferencesWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
