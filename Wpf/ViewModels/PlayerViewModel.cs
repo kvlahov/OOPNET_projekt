510399e4b781d2ba389ffee9132ea950fad38e07
Kresimir Vlahov <kresimir.vlahov@ericsson.com>
2020-05-11 21:29:01 +0200

    Add PlayerViewModel

---
+++ Wpf/ViewModels/PlayerViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities.Model;
+
+namespace Wpf.ViewModels
+{
+    public class PlayerViewModel : ViewModelBase
+    {
+		private int _playerNumber;
+		public int PlayerNumber
+		{
+			get { return _playerNumber; }
+			set { SetProperty(ref _playerNumber, value); }
+		}
+
+		private string _playerName;
+		public string PlayerName
+		{
+			get { return _playerName; }
+			set { SetProperty(ref _playerName, value); }
+		}
+
+		private bool _isCaptain;
+		public bool IsCaptain
+		{
+			get { return _isCaptain; }
+			set { SetProperty(ref _isCaptain, value); }
+		}
+
+		private bool _isHomeTeam;
+		public bool IsHomeTeam
+		{
+			get { return _isHomeTeam; }
+			set { SetProperty(ref _isHomeTeam, value); }
+		}
+
+		private Position _position;
+		public Position Position
+		{
+			get { return _position; }
+			set { SetProperty(ref _position, value); }
+		}
+
+		private int _noOfYellowCards;
+		public int NoOfYellowCards
+		{
+			get { return _noOfYellowCards; }
+			set { SetProperty(ref _noOfYellowCards, value); }
+		}
+
+	}
+}
